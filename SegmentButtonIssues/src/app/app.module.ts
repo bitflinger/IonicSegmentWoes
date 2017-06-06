d20132711cdb46c8c6e2bd6f37a890af7a8ea5ff
Charlie M <bitflinger@hotmail.com>
2017-06-06 12:22:10 -0500

    Add project files.

---
+++ SegmentButtonIssues/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+      BrowserModule,
+   IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
---
+++ SegmentButtonIssues/src/pages/home/home.ts
@@ -0,0 +1,47 @@
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { NavParams, ViewController, Platform } from 'ionic-angular';
+import { FormBuilder, Validators, FormGroup, FormControl, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+    private editFormGroup: FormGroup;
+    private drinkTypes;
+
+    private drink = {
+        drinkType: {
+            id: 2,
+            name: 'tea'
+        }
+    };
+
+
+    constructor(private params: NavParams, public viewCtrl: ViewController, private formBuilder: FormBuilder,
+                 private changeDetection: ChangeDetectorRef) {
+
+
+  }
+  
+    ngOnInit() {
+        this.editFormGroup = this.formBuilder.group({
+            "drinkTypeName": new FormControl({ value: this.drink.drinkType.name }, Validators.required),
+        });
+        this.getDrinkTypesFromDataBase();
+    }
+
+
+    getDrinkTypesFromDataBase() {
+       // this.drinkTypes = [{ id: 1, name: 'coffee' }, { id: 2, name: 'tea' }, { id: 3, name: 'milk' }]; //HERE, IT WORKS
+
+        setTimeout(() => {
+            this.drinkTypes = [{ id: 1, name: 'coffee' }, { id: 2, name: 'tea' }, { id: 3, name: 'milk' }]; //HERE iT DIES
+        }, 100)
+        
+    }
+
+}
